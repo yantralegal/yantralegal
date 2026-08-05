9442f4505ad7bf297eb15a40beb6ea49584600e4
yantralegal <yantralegalweb@gmail.com>
2026-08-05 22:30:40 +0545

    feat: integrate Google Analytics and Microsoft Clarity tracking scripts into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -164,6 +164,28 @@ export default async function RootLayout({
         />
       </head>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9YXZYMF6CD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9YXZYMF6CD');
+          `}
+        </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xxprhxjeuy");
+          `}
+        </Script>
         {showMaintenance ? (
           <div style={{
             minHeight: '100vh',
